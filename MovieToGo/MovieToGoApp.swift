cf51e674160f371847e99f57a063d6c3013312f4
Artem Tkachenko <tkachenkartm@gmail.com>
2024-12-02 20:33:27 +0200

    Initial Commit

---
+++ MovieToGo/MovieToGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieToGoApp.swift
+//  MovieToGo
+//
+//  Created by Artem Tkachenko on 12/2/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieToGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
